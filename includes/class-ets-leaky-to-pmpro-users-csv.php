06ef9af13f5eeae52493c4377ccae4d236865d46
Younes DRO <dro66_8@hotmail.fr>
2024-01-30 12:16:34 +0100

    Get Leaky Paywall payment transaction ID for a user.

--- includes/class-ets-leaky-to-pmpro-users-csv.php
+++ includes/class-ets-leaky-to-pmpro-users-csv.php
@@ -21,15 +21,15 @@
  */
 class ETS_LeakyToPMPro_Users_CSV {
 
-     /**
-     * The single instance of the class.
-     *
-     * @var self
-     */
-    private static $instance;
+	 /**
+	  * The single instance of the class.
+	  *
+	  * @var self
+	  */
+	private static $instance;
 
 
-    /**
+	/**
 	 * Folder to save CSV
 	 *
 	 * @since    1.0.0
@@ -38,27 +38,27 @@ class ETS_LeakyToPMPro_Users_CSV {
 	 */
 	private $csv_folder;
 
-    /**
-     * The current mode obtained using leaky_paywall_get_current_mode().
-     *
-     * @var string
-     */
-    public $mode;
-    
-    /**
-     * Main ETS_LeakyToPMPro_Users_CSV instance.
-     *
-     * Ensures only one instance of the class is loaded or can be loaded.
-     *
-     * @return self Instance of the class.
-     */
-    public static function get_instance() {
-        if ( null === self::$instance ) {
-        self::$instance = new self();
-        }
+	/**
+	 * The current mode obtained using leaky_paywall_get_current_mode().
+	 *
+	 * @var string
+	 */
+	public $mode;
 
-        return self::$instance;
-    }
+	/**
+	 * Main ETS_LeakyToPMPro_Users_CSV instance.
+	 *
+	 * Ensures only one instance of the class is loaded or can be loaded.
+	 *
+	 * @return self Instance of the class.
+	 */
+	public static function get_instance() {
+		if ( null === self::$instance ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
 
 	/**
 	 * Constructor for the class.
@@ -67,50 +67,50 @@ class ETS_LeakyToPMPro_Users_CSV {
 	 *
 	 * @since 1.0.0
 	 */
-    private function __construct() {
+	private function __construct() {
 		$this->csv_folder = trailingslashit( wp_upload_dir()['basedir'] ) . ETS_LEAKY_TO_PMPRO_CSV_FOLDER;
 
 		if ( ! is_dir( $this->csv_folder ) ) {
 			wp_mkdir_p( $this->csv_folder );
 			chmod( $this->csv_folder, 0777 );
 		}
-        $this->mode = $this->get_current_mode();
-    }
+		$this->mode = $this->get_current_mode();
+	}
 
-    /***
-     * 
-     * 
-     */
-    private function get_current_mode() {
-        $settings = new Leaky_Paywall_Settings();
-        $s = $settings->get_settings();
-        $mode     = 'off' === $s['test_mode'] ? 'live' : 'test';
+	/***
+	 *
+	 *
+	 */
+	private function get_current_mode() {
+		$settings = new Leaky_Paywall_Settings();
+		$s        = $settings->get_settings();
+		$mode     = 'off' === $s['test_mode'] ? 'live' : 'test';
 
-        return $mode;
-    }
+		return $mode;
+	}
 
-    /**
-     * Initialize hooks.
-     * 
-     * @return void
-     */
-    public function init() {
-        add_action( 'wp_ajax_ets_leaky_to_pmpro_generate_csv', array( $this, 'generate_csv' ), 10 );
-    }
+	/**
+	 * Initialize hooks.
+	 *
+	 * @return void
+	 */
+	public function init() {
+		add_action( 'wp_ajax_ets_leaky_to_pmpro_generate_csv', array( $this, 'generate_csv' ), 10 );
+	}
 
-    /**
-     * Generate CSV file for user migration.
-     * 
-     * Functions to use : 
-     * - leaky_paywall_user_has_access()
-     * - leaky_paywall_subscriber_current_level_id() // Return level id for user
-     */
-    public function generate_csv() {
-        global $wpdb;
-        $mode      = $this->mode;
+	/**
+	 * Generate CSV file for user migration.
+	 *
+	 * Functions to use :
+	 * - leaky_paywall_user_has_access()
+	 * - leaky_paywall_subscriber_current_level_id() // Return level id for user
+	 */
+	public function generate_csv() {
+		global $wpdb;
+		$mode = $this->mode;
 
-        $sql = $wpdb->prepare(
-            "
+		$sql = $wpdb->prepare(
+			"
             SELECT
                 u.ID as user_id,
                 u.user_login,
@@ -127,102 +127,99 @@ class ETS_LeakyToPMPro_Users_CSV {
                 AND um.meta_value = %s
                 AND um.meta_key LIKE %s
             ",
-            '_issuem_leaky_paywall_%',
-            '%_payment_status',
-            'active',
-            '_issuem_leaky_paywall_' . $mode . '%'
-        );
+			'_issuem_leaky_paywall_%',
+			'%_payment_status',
+			'active',
+			'_issuem_leaky_paywall_' . $mode . '%'
+		);
 
-        $results = $wpdb->get_results($sql, ARRAY_A);
+		update_option( 'psypost_sql', $sql );
 
-        $date     = date( 'd-m-y-' . substr( ( string ) microtime(), 1, 8 ) );
-        $date     = str_replace( '.', '', $date );
-        $filename = 'ets-leaky-pmpro-subscribers-' . $date . '.csv';
-        $filePath = $this->csv_folder . '/' . $filename;
-        $handle   = fopen( $filePath, 'w' );
+		$results = $wpdb->get_results( $sql, ARRAY_A );
 
-        fputs($handle, "\xEF\xBB\xBF"); // UTF-8 BOM
+		$date     = date( 'd-m-y-' . substr( (string) microtime(), 1, 8 ) );
+		$date     = str_replace( '.', '', $date );
+		$filename = 'ets-leaky-pmpro-subscribers-' . $date . '.csv';
+		$filePath = $this->csv_folder . '/' . $filename;
+		$handle   = fopen( $filePath, 'w' );
 
-        $headers = array(
-            'user_login',
-            'user_email',
-            'user_pass',
-            'first_name',
-            'last_name',
-            'display_name',
-            'role',
-            'membership_id',
-            'membership_code_id',
-            'membership_initial_payment',
-            'membership_billing_amount',
-            'membership_cycle_number',
-            'membership_cycle_period',
-            'membership_billing_limit',
-            'membership_trial_amount',
-            'membership_trial_limit',
-            'membership_status',
-            'membership_startdate',
-            'membership_enddate',
-            'membership_subscription_transaction_id',
-            'membership_gateway',
-            'membership_payment_transaction_id',
-            'membership_affiliate_id',
-            'membership_timestamp',
-        );
-        
+		fputs( $handle, "\xEF\xBB\xBF" ); // UTF-8 BOM
 
-        fputcsv( $handle, $headers );
+		$headers = array(
+			'user_login',
+			'user_email',
+			'user_pass',
+			'first_name',
+			'last_name',
+			'display_name',
+			'role',
+			'membership_id',
+			'membership_code_id',
+			'membership_initial_payment',
+			'membership_billing_amount',
+			'membership_cycle_number',
+			'membership_cycle_period',
+			'membership_billing_limit',
+			'membership_trial_amount',
+			'membership_trial_limit',
+			'membership_status',
+			'membership_startdate',
+			'membership_enddate',
+			'membership_subscription_transaction_id',
+			'membership_gateway',
+			'membership_payment_transaction_id',
+			'membership_affiliate_id',
+			'membership_timestamp',
+		);
 
-        foreach ($results as $result) {
-            $user_id = $result['user_id'];
-            $user                     = new WP_User( $user_id );
-            if ( ! $user->exists() ) {
-                continue;
-            }
+		fputcsv( $handle, $headers );
 
-            $row = array(
-                $user->user_login,
-                $user->user_email,
-                $user->user_pass,
-                $user->first_name,
-                $user->last_name,
-                $user->display_name,
-                $this->get_member_role( $user_id ),
-                $this->get_membership_id( $user_id ),
-                '',  // membership_code_id - not available in Leaky Paywall
-                $this->get_membership_initial_payment( $user_id ),
-                $this->get_membership_timestamp( $user_id),
-                $this->get_membership_billing_amount( $user_id),
-                $this->get_membership_cycle_number(),
-                $this->get_membership_cycle_period(),
-                '',  // membership_billing_limit - not available in Leaky Paywall
-                '',  // membership_trial_amount - not available in Leaky Paywall
-                '',  // membership_trial_limit - not available in Leaky Paywall
-                $this->get_member_status( $user_id ),
-                $this->get_membership_startdate(),
-                $this->get_membership_enddate(),
-                $this->get_membership_timestamp( $user_id),
-                $this->get_membership_subscription_transaction_id(),
-                $this->get_membership_gateway(),
-                $this->get_membership_payment_transaction_id(),
-                '',  // membership_affiliate_id - not available in Leaky Paywall
-                '',  // membership_timestamp - not available in Leaky Paywall
-            );
-            
+		foreach ( $results as $result ) {
+			$user_id = $result['user_id'];
+			$user    = new WP_User( $user_id );
+			if ( ! $user->exists() ) {
+				continue;
+			}
 
-            
-            fputcsv( $handle, $row );
+			$row = array(
+				$user->user_login,
+				$user->user_email,
+				$user->user_pass,
+				$user->first_name,
+				$user->last_name,
+				$user->display_name,
+				$this->get_member_role( $user_id ),
+				$this->get_membership_id( $user_id ),
+				'',  // membership_code_id - not available in Leaky Paywall
+				$this->get_membership_initial_payment( $user_id ),
+				$this->get_membership_billing_amount( $user_id ),
+				$this->get_membership_cycle_number(),
+				$this->get_membership_cycle_period(),
+				'',  // membership_billing_limit - not available in Leaky Paywall
+				$this->get_membership_trial_amount(),
+				'',  // membership_trial_limit - not available in Leaky Paywall
+				$this->get_member_status( $user_id ),
+				$this->get_membership_startdate( $user_id ),
+				$this->get_membership_enddate( $user_id ),
+				$this->get_membership_subscription_transaction_id( $user_id ),
+				$this->get_membership_gateway(),
+				$this->get_membership_payment_transaction_id(),
+				'',  // membership_affiliate_id - not available in Leaky Paywall
+				$this->get_membership_timestamp( $user_id ),  // membership_timestamp - not available in Leaky Paywall
+			);
 
-        }
+			fputcsv( $handle, $row );
 
-        fclose($handle);
-        $upload_dir = wp_upload_dir();
-        echo '<a href="' . $upload_dir['baseurl'] . '/' . ETS_LEAKY_TO_PMPRO_CSV_FOLDER . '/' . $filename . '"><span class="dashicons dashicons-download"></span>Download : ' . $filename . '</a>';
+		}
 
-        exit();
-    }
+		fclose( $handle );
+		$upload_dir = wp_upload_dir();
+		echo '<a href="' . $upload_dir['baseurl'] . '/' . ETS_LEAKY_TO_PMPRO_CSV_FOLDER . '/' . $filename . '"><span class="dashicons dashicons-download"></span>Download : ' . $filename . '</a>';
 
-    /**
+		exit();
+	}
+
+	/**
 	 * Get Member first Role.
 	 *
 	 * @param int $user_id User ID.
@@ -231,7 +228,7 @@ class ETS_LeakyToPMPro_Users_CSV {
 	 */
 	private function get_member_role( $user_id ) {
 
-        return 'subscriber';
+		return 'subscriber';
 
 		// $user_data = get_userdata( $user_id );
 		// $role      = ! empty( $user_data->roles[0] ) ? $user_data->roles[0] : 'none';
@@ -242,21 +239,21 @@ class ETS_LeakyToPMPro_Users_CSV {
 	/**
 	 * Get level ID for a user.
 	 *
-	 * @param int           $user_id    User ID.
+	 * @param int $user_id    User ID.
 	 *
 	 * @return int $level_id.
 	 */
-	private function get_membership_id( $user_id  ) {
+	private function get_membership_id( $user_id ) {
 
-        $level_id = get_user_meta( $user_id, '_issuem_leaky_paywall_' . $this->mode . '_level_id' , true );
+		$level_id = get_user_meta( $user_id, '_issuem_leaky_paywall_' . $this->mode . '_level_id', true );
 
-        if ( is_numeric( $level_id ) ) {
-            return $level_id;
-        }
-    } 
-    
-    /**
-	 * Get Price 
+		if ( is_numeric( $level_id ) ) {
+			return $level_id;
+		}
+	}
+
+	/**
+	 * Get Price
 	 *
 	 * @param int $user_id User ID.
 	 *
@@ -264,100 +261,183 @@ class ETS_LeakyToPMPro_Users_CSV {
 	 */
 	private function get_membership_initial_payment( $user_id ) {
 
-        $price = get_user_meta( $user_id, '_issuem_leaky_paywall_' . $this->mode . '_price', true );
-         return $price;
-    }
+		$price = get_user_meta( $user_id, '_issuem_leaky_paywall_' . $this->mode . '_price', true );
+		 return $price;
+	}
 
-    /**
-     * Undocumented function
-     *
-     * @param [type] $user_id
-     * @return void
-     */
-    private function get_membership_timestamp( $user_id ){
-        $created = get_user_meta( $user_id, '_issuem_leaky_paywall_' . $this->mode . '_created', true );
-        return $created;
-    }
+	/**
+	 * Undocumented function
+	 *
+	 * @param [type] $user_id
+	 * @return void
+	 */
+	private function get_membership_timestamp( $user_id ) {
+		$created        = get_user_meta( $user_id, '_issuem_leaky_paywall_' . $this->mode . '_created', true );
+		$timestamp      = strtotime( $created );
+		$year           = date( 'Y', $timestamp );
+		$month          = date( 'm', $timestamp );
+		$day            = date( 'd', $timestamp );
+		$formatted_date = $year . '-' . $month . '-' . $day;
 
-    /**
-     * Undocumented function
-     *
-     * @param [type] $user_id
-     * @return void
-     */
-    private function get_membership_billing_amount( $user_id ) {
-        $level_id = $this->get_membership_id( $user_id );
-        $allsettings = get_option( 'issuem-leaky-paywall' );
-        $levels = $allsettings['levels'];
+		return $formatted_date;
+	}
 
-        return $levels[ $level_id ]['price'];
-    }
+	/**
+	 * Undocumented function
+	 *
+	 * @param [type] $user_id
+	 * @return void
+	 */
+	private function get_membership_billing_amount( $user_id ) {
+		$level_id    = $this->get_membership_id( $user_id );
+		$allsettings = get_option( 'issuem-leaky-paywall' );
+		$levels      = $allsettings['levels'];
 
-    /**
-     * Get membership cycle number.
-     *
-     * @return string
-     */
-    private function get_membership_cycle_number() {
-        
-        return '';
-    }
+		return $levels[ $level_id ]['price'];
+	}
 
-    /**
-     * Get membership cycle period.
-     *
-     * @return string
-     */
-    private function get_membership_cycle_period() {
-        
-        return '';
-    }
+	/**
+	 * Get membership cycle number.
+	 *
+	 * @return string
+	 */
+	private function get_membership_cycle_number() {
 
-    /**
-     * Get membership billing limit.
-     *
-     * @return string
-     */
-    private function get_membership_billing_limit() {
-        
-        return '';
-    }
+		return 1;
+	}
 
-    /**
-     * Get membership trial amount.
-     *
-     * @return string
-     */
-    private function get_membership_trial_amount() {
-        
-        return '';
+	/**
+	 * Get membership cycle period.
+	 *
+	 * @return string
+	 */
+	private function get_membership_cycle_period() {
 
-    }
+		return 'Month';
+	}
 
-    private function get_member_status(){
-        return '';
-    }
-    private function get_membership_startdate() {
-        return '';
-    }
-    
-    private function get_membership_enddate() {
-        return '';
-    }
-    
-    
-    private function get_membership_subscription_transaction_id() {
-        return '';
-    }
-    
-    private function get_membership_gateway() {
-        return '';
-    }
-    
-    private function get_membership_payment_transaction_id() {
-        return '';
-    }
-        
+	/**
+	 * Get membership billing limit.
+	 *
+	 * @return string
+	 */
+	private function get_membership_billing_limit() {
+
+		return '';
+	}
+
+	/**
+	 * Get membership trial amount.
+	 *
+	 * @return init
+	 */
+	private function get_membership_trial_amount() {
+
+		return '-2.00';
+
+	}
+
+	/**
+	 * Undocumented function
+	 *
+	 * @return string
+	 */
+	private function get_member_status() {
+		$status = get_user_meta( $user_id, '_issuem_leaky_paywall_' . $this->mode . '_payment_status', true );
+		return $status;
+
+	}
+	/**
+	 * Undocumented function
+	 *
+	 * @return void
+	 */
+	private function get_membership_startdate( $user_id ) {
+		$created = get_user_meta( $user_id, '_issuem_leaky_paywall_' . $this->mode . '_created', true );
+		update_option( 'leaky_date_created', $created );
+		// $formatted_date = date( 'Y-m-d', strtotime( $created ) );
+		$timestamp      = strtotime( $created );
+		$year           = date( 'Y', $timestamp );
+		$month          = date( 'm', $timestamp );
+		$day            = date( 'd', $timestamp );
+		$formatted_date = $year . '-' . $month . '-' . $day;
+		return $formatted_date;
+
+	}
+
+	/**
+	 * Undocumented function
+	 *
+	 * @return void
+	 */
+	private function get_membership_enddate( $user_id ) {
+		$expires = get_user_meta( $user_id, '_issuem_leaky_paywall_' . $this->mode . '_expires', true );
+		update_option( 'leaky_date_expires', $expires );
+		// $formatted_date = date( 'Y-m-d', strtotime( $expires ) );
+		$timestamp      = strtotime( $expires );
+		$year           = date( 'Y', $timestamp );
+		$month          = date( 'm', $timestamp );
+		$day            = date( 'd', $timestamp );
+		$formatted_date = $year . '-' . $month . '-' . $day;
+		return $formatted_date;
+	}
+
+
+	/**
+	 * Undocumented function
+	 *
+	 * @param [type] $user_id
+	 * @return void
+	 */
+	private function get_membership_subscription_transaction_id( $user_id ) {
+		$subscription_transaction_id = get_user_meta( $user_id, '_issuem_leaky_paywall_' . $this->mode . '_subscriber_id', true );
+
+		return $subscription_transaction_id;
+	}
+
+	/**
+	 * Undocumented function
+	 *
+	 * @return void
+	 */
+	private function get_membership_gateway() {
+		return 'stripe';
+	}
+
+
+	/**
+	 * Get Leaky Paywall payment transaction ID for a user.
+	 *
+	 * @param int $user_id User ID.
+	 * @return string|false Payment transaction ID or false if not found.
+	 */
+	private function get_membership_payment_transaction_id( $user_id ) {
+
+		$entries = get_posts(
+			array(
+				'post_type'  => 'lp_transaction',
+				'meta_query' => array(
+					array(
+						'key'   => '_email',
+						'value' => get_userdata( $user_id )->user_email,
+					),
+				),
+				'fields'     => 'ids',
+				'orderby'    => 'post_date',
+				'order'      => 'DESC',
+			)
+		);
+
+		foreach ( $entries as $entry_id ) {
+			$payment_transaction_id = get_post_meta( $entry_id, '_gateway_txn_id', true );
+
+			if ( ! empty( $payment_transaction_id ) ) {
+				return $payment_transaction_id;
+			}
+		}
+		return '';
+	}
+
 
 }
 
